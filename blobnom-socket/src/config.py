1836e6631ceccc866fd8bea3cd9313716e173bc3
pgh <pggggggggh@yonsei.ac.kr>
2025-12-31 23:34:21 +0900

    chore: prepare for deployment

--- blobnom-socket/src/config.py
+++ blobnom-socket/src/config.py
@@ -1,3 +1,3 @@
 import os
 
-REDIS_URL = f"redis://{os.environ.get('REDIS_HOST', 'localhost')}:{os.environ.get('REDIS_PORT', '6379')}/{os.environ.get('REDIS_DBNO', '0')}"
+REDIS_URL = os.environ.get("REDIS_URL")
